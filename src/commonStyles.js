727e1cd6d09b6de6b47bd61852aa8dbd70af7533
alanhcrdz <alanhcrdz@gmail.com>
2021-01-20 15:37:21 -0300

    initial commit

---
+++ src/commonStyles.js
@@ -0,0 +1,12 @@
+export default {
+    fontFamily: 'Lato',
+    colors: {
+        today: '#b13b44',
+        tomorrow: "#c9742e",
+        week: "#15721e",
+        month: "#1631be",
+        secondary: '#fff',
+        mainText: 'grey',
+        subText: '#555'
+    }
+}
